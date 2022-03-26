2bd279868d1849bb2ec0f2be10be1dedd99da9c3
Raza Mahmood <razammmahmood@gmail.com>
2022-03-25 21:30:16 -0400

    updating favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,14 +5,27 @@ const config: GatsbyConfig = {
     title: ``,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "./src/images/icon.png"
+      }
+    }
+  ]
 };
 
 export default config;
