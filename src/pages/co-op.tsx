b383d8b8976b44a4393ffd4ab9193a5e16c922d0
Raza Mahmood <razammmahmood@gmail.com>
2022-04-09 19:01:53 -0400

    adding co-op page

---
+++ src/pages/co-op.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Page from "../components/Page";
+import styled from "styled-components";
+
+export default function IndexPage() {
+    return (
+        <Page title="Co-op Work">
+
+        </Page>
+    )
+}
